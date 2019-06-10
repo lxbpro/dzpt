7ebe154a5064c89cb5573c202a16468d89beb3ed
gao380221829 <380221829@qq.com>
2019-06-10 16:18:14 +0800

    Revert "测试"
    This reverts commit 3a62d682

--- app/src/main/java/com/gamecenter/gamecenter/model/UserModel.java
+++ app/src/main/java/com/gamecenter/gamecenter/model/UserModel.java
@@ -8,7 +8,7 @@ import org.json.JSONObject;
 
 public class UserModel {
     private int id;
-    private String username;
+    private String username;222
     private String account;
     private String pwd;
     private String email;
